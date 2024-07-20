ed0c6961f4783b06366f18478135a87f82167609
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-07-20 12:11:39 +0330

    l

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+
+namespace HelloWorld
+{
+    public class Person
+    {
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item = new Person() { FirstName = "Foo", LastName = "Bar"};
+            var serializedItem = JsonSerializer.Serialize(item);
+            Console.WriteLine(serializedItem);
+        }
+    }
+}
---
+++ app/test-library/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace test_library;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
